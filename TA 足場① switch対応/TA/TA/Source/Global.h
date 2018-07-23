7bb17df87b546e79fe1a161b9e8652d2f9a52912
pe870 <hub.shinpei@gmail.com>
2018-07-23 12:04:25 +0900

    TA 足場⑴switch文対応
    四角上下移動switch文に変更

---
+++ TA 足場① switch対応/TA/TA/Source/Global.h
@@ -0,0 +1,26 @@
+#ifndef GLOBAL_H	//²İ¸Ù°ÄŞ¶Ş°ÄŞ.
+#define GLOBAL_H
+
+//Œx‚É‚Â‚¢‚Ä‚Ìº°ÄŞ•ªÍ‚ğ–³Œø‚É‚·‚é. 4005:Ä’è‹`.
+#pragma warning(disable:4005)
+
+#include <Windows.h>
+#include <D3DX11.h>
+#include <D3D11.h>
+#include <D3DX10.h>
+#include <D3D10.h>
+
+//×²ÌŞ×Ø.
+#pragma comment( lib, "winmm.lib" )
+#pragma comment( lib, "d3dx11.lib" )
+#pragma comment( lib, "d3d11.lib" )
+#pragma comment( lib, "d3dx10.lib" )
+
+
+//’è”.
+#define WND_W	1280	//³¨İÄŞ³•.
+#define WND_H	720		//³¨İÄŞ³‚‚³.
+
+
+
+#endif//#ifndef GLOBAL_H
---
+++ TA 足場① switch対応/TA/TA/Source/Position.h
@@ -0,0 +1,33 @@
+#ifndef POSITION_H
+#define POSITION_H
+
+#pragma warning(disable:4005)
+//LoadModelのm_vPosはここの定数を使用しているため.
+//結合の際、必ず変更してビルドすること.
+
+//Default Model Position.
+const float GroundX =  0.0f;
+const float GroundY = -0.5f;
+const float GroundZ = -5.7f;
+
+const float PlayerX =  0.0f;
+const float PlayerY =  0.8f;
+const float PlayerZ = -8.0f;
+
+/***********①***********/
+int scaffoldCntSqu = 0;
+int Italy = L3;
+const float scaffoldSquX = -1.7f;
+const float scaffoldSquY = 0.1f;
+const float scaffoldSquZ = 7.0f;
+bool moveOne = false;
+bool moveTwo = false;
+
+const float scaffoldSqumX = 1.7f;
+const float scaffoldSqumY = 4.2f;
+const float scaffoldSqumZ = 11.0f;
+
+
+/***********②***********/
+
+#endif
